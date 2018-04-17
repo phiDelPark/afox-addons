076959be7674c0091fef16fb0fd273b72abc62fe
phiDel <phidel@afox.kr>
2018-04-17 14:10:28 +0900

    image_downloader 에드온 추가

---
+++ addon/image_downloader/index.php
@@ -0,0 +1,92 @@
+<?php
+if (!defined('__AFOX__')) exit();
+
+if ($called_position == 'after_proc' && $called_trigger == 'updatedocument' && !empty($_DATA['wr_srl'])) {
+
+	$doc = DB::get(_AF_DOCUMENT_TABLE_, 'md_id,wr_content,wr_file,mb_srl,mb_ipaddress', ['wr_srl'=>$_DATA['wr_srl']]);
+	if (!empty($doc)) {
+		$wr_srl = $_DATA['wr_srl'];
+		$md_id = $doc['md_id'];
+		$mb_srl = $doc['mb_srl'];
+		$mb_ipaddress = $doc['mb_ipaddress'];
+
+		$file_count = (int)$doc['wr_file'];
+		$_check = [];
+
+		if (empty($_ADDON['except'])) $_ADDON['except'] = 'img.youtube.com';
+		$except = str_replace("\n", '|', trim($_SERVER['HTTP_HOST'] . "\n" . $_ADDON['except']));
+
+		$content = preg_replace_callback(
+			'/(<img[^>]*?\s?src\s*=\s*["\']?)(https?:[^>"\']*)/i',
+			function ($m) use ($md_id, $wr_srl, $mb_srl, $mb_ipaddress, $except, &$file_count, &$_check) {
+				$url = $m[2];
+
+				if(preg_match('/^https?:[\/]+('. $except .')/i', $url)) {
+					return $m[1].$url;
+				}
+
+				if(empty($_check[$url])) {
+
+					$parts = pathinfo(parse_url($url, PHP_URL_PATH));
+
+					$iname = $parts['filename'];
+					$iext = explode('?', empty($parts['extension']) ? '' : $parts['extension']);
+					$iext = '.' . (empty($iext[0]) ? 'none' : $iext[0]);
+
+					// Immediately remove the direct file if it has any kind of extensions for hacking
+					$iext = preg_replace('/\.(php|phtm|phar|html?|cgi|pl|exe|jsp|asp|inc)/i', '$0-x', $iext);
+
+					$upload_name = md5($iname . time() . count($_check)) . $iext;
+					$uploaded_filename = _AF_ATTACH_DATA_ . 'image/' . $md_id . '/' . $wr_srl . '/' . $upload_name;
+
+					$_check[$url] = $url . '" data-error="download-failure';
+
+					// 폴더 없으면 만듬
+					$dir = dirname($uploaded_filename);
+					if(!is_dir($dir) && !mkdir($dir, _AF_DIR_PERMIT_, true)) {
+						return $m[1].$_check[$url];
+					}
+
+					@copy($url, $uploaded_filename);
+					if(file_exists($uploaded_filename)) {
+						@chmod($uploaded_filename, _AF_FILE_PERMIT_);
+
+						// 공개 하게되면  크기 체크기능 넣자
+						// size-limit-single
+						// size-limit-total
+
+						if(DB::insert(_AF_FILE_TABLE_, [
+							'md_id'=>$md_id,
+							'mf_target'=>$wr_srl,
+							'mf_name'=>$iname . $iext,
+							'mf_upload_name'=>$upload_name,
+							'mf_size'=>filesize($uploaded_filename),
+							'mf_type'=>'image/' . substr($iext, 1),
+							'mb_srl'=>$mb_srl,
+							'mb_ipaddress'=>$mb_ipaddress,
+							'^mf_regdate'=>'NOW()'
+						]) === true) {
+							$mf_srl = DB::insertId();
+							$file_count++;
+							$_check[$url] = './?file=' . $mf_srl;
+						} else {
+							unlinkFile($uploaded_filename);
+							$_check[$url] = $url . '" data-error="insert-error';
+						}
+					}
+
+				}
+
+				return $m[1].$_check[$url];
+			},
+			$doc['wr_content']
+		);
+
+		if($file_count > 0) {
+			DB::update(_AF_DOCUMENT_TABLE_, ['wr_content'=>$content,'wr_file'=>$file_count], ['wr_srl'=>$wr_srl]);
+		}
+	}
+}
+
+/* End of file index.php */
+/* Location: ./addon/image_downloader/index.php */
---
+++ addon/image_downloader/setup.php
@@ -0,0 +1,10 @@
+<?php
+if(!defined('__AFOX__')) exit();
+$default_except = 'img.youtube.com';
+?>
+
+<div class="form-group">
+	<label for="id_except">제외</label>
+	<textarea maxbyte="65000" maxlength="65000" class="form-control mh-10 vresize" name="except" id="id_except"><?php echo escapeHtml(empty($_ADDON['except'])?$default_except:$_ADDON['except'])?></textarea>
+	<p class="help-block">입력된 호스트의 이미지는 다운로드에서 제외합니다. (호스트 사이에 구분은 엔터(Enter)로 합니다)</p>
+</div>
