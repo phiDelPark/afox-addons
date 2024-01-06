bba129f371c826c6a8033726ac6d738a047bafe7
phiDelPark <apps.afox@gmail.com>
2024-01-06 15:49:32 +0900

    CSS, JS 압축

--- addon/web_code/index.php
+++ addon/web_code/index.php
@@ -3,8 +3,8 @@ if(!defined('__AFOX__')) exit();
 
 if($called_position == 'after_disp' && $called_trigger == 'default' && !empty($_DATA['wr_content'])) {
 	if(preg_match('/<blockquote[^>]*web-code="area"/', $_DATA['wr_content'])){
-		addCSS(_AF_URL_.'addon/web_code/web_code'. (__DEBUG__ ? '.css?' . _AF_SERVER_TIME_ : '.min.css'));
-		addJS(_AF_URL_.'addon/web_code/web_code'. (__DEBUG__ ? '.js?' . _AF_SERVER_TIME_ : '.min.js?w='.(empty($_ADDON['pre-wrap'])?'0':'1')));
+		addCSS(_AF_URL_.'addon/web_code/web_code'.(__DEBUG__ ? '' : '.min').'.css');
+		addJS(_AF_URL_.'addon/web_code/web_code'.(__DEBUG__ ? '' : '.min').'.js?');
 	}
 }
 
