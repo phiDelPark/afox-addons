da56257c94c23afbb1e66b9de6aca0aefc332028
phiDelPark <apps.afox@gmail.com>
2024-02-03 04:23:31 +0900

    Description 필드 이름 변경

--- addon/code_highlighter/info.php
+++ addon/code_highlighter/info.php
@@ -6,7 +6,7 @@ $_ADDON_INFO['author'] = "aFox";
 $_ADDON_INFO['email'] = "support@a.fox";
 $_ADDON_INFO['link'] = "https://github.com/phiDelPark/afox-addons";
 $_ADDON_INFO['title'] = "코드 문법강조";
-$_ADDON_INFO['description'] = "문서 내 코드들을 문법강조 해줍니다. (PHP, HTML, C... 등등)";
+$_ADDON_INFO['about'] = "문서 내 코드들을 문법강조 해줍니다. (PHP, HTML, C... 등등)";
 $_ADDON_INFO['date'] = "2018-02-05";
 
 /* End of file info.php */
